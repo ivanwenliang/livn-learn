8b97c0a2c1f58c89c4ffb6ff6fc9c5ae66c2f74d
Ivan Liang <ivanwenliang@gmail.com>
2019-10-19 17:32:31 -0700

    Add course name on course detail page and not found page

--- src/pages/CourseDetailPage.js
+++ src/pages/CourseDetailPage.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { connect } from "react-redux";
+import NotFoundPage from "./NotFoundPage";
 
-const CourseDetailPage = ({ courseId }) => {
-  return <div>Viewing {courseId}</div>;
+const CourseDetailPage = ({ courseId, course, loading }) => {
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!course) {
+    return <NotFoundPage />;
+  }
+
+  return (
+    <div>
+      Viewing {courseId} -- {course.name}
+    </div>
+  );
 };
 
-export default CourseDetailPage;
+const mapState = (state, ownProps) => ({
+  loading: state.coursesLoading,
+  course: state.courses.find(
+    course => course.id === parseInt(ownProps.courseId)
+  )
+});
+export default connect(mapState)(CourseDetailPage);
---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const NotFoundPage = () => (
+  <div style={{ margin: 20 }}>
+    <h1>404 Not Found</h1>
+    <p>So Sorry.</p>
+  </div>
+);
+
+export default NotFoundPage;
