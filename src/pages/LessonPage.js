889363e88588b883d409c2c1074d36699fbf1491
Ivan Liang <ivanwenliang@gmail.com>
2019-10-24 13:06:20 -0700

    Create lesson editor page

--- src/pages/LessonPage.js
+++ src/pages/LessonPage.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { connect } from "react-redux";
+import LessonEditor from "../components/LessonEditor";
+import NotFoundPage from "./NotFoundPage";
 
-const LessonPage = ({ lessonId }) => {
-  return <div>{lessonId}</div>;
+const LessonPage = ({ lesson, loading }) => {
+  if (loading) {
+    return "Loading...";
+  }
+  if (!lesson) {
+    return <NotFoundPage />;
+  }
+  return <LessonEditor lesson={lesson} />;
 };
 
-export default LessonPage;
+const mapState = (state, props) => {
+  const lessonId = parseInt(props.lessonId, 10);
+  return {
+    lesson: state.lessons.lessons[lessonId],
+    loading: state.lessons.loading
+  };
+};
+export default connect(mapState)(LessonPage);
