eb382fbcdd46ed5197df7e5461e92333a767b990
Ivan Liang <ivanwenliang@gmail.com>
2019-10-25 16:57:54 -0700

    Persist user object to localStorage

--- src/middleware.js
+++ src/middleware.js
@@ -3,8 +3,10 @@ import { setToken } from "./api";
 
 export const saveAuthToken = store => next => action => {
   if (action.type === LOGIN_SUCCESS || action.type === SIGNUP_SUCCESS) {
-    // save token
+    // Save token
     setToken(action.payload.token);
+    // Persist token to localStorage so token remains after refreshing
+    localStorage.setItem("currentUser", JSON.stringify(action.payload));
   }
   next(action);
 };
