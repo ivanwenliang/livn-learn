deb01e96c56831a35b414930357a698e385d5d93
Ivan Liang <ivanwenliang@gmail.com>
2019-10-26 14:17:11 -0700

    Update buy now button to work

--- src/pages/SalesPage.js
+++ src/pages/SalesPage.js
@@ -1,19 +1,29 @@
 import React from "react";
 import { connect } from "react-redux";
-import "./SalesPage.css";
+import { navigate } from "@reach/router";
 import { getCourseById } from "../selectors";
+import "./SalesPage.css";
 
-const SalesPage = ({ course }) => {
+// Use navigate instead of redirect, because SalesPage is a top-level route
+const SalesPage = ({ course, currentUser, courseId, navigate }) => {
+  const buyOrLogin = () => {
+    if (currentUser) {
+      buyCourse(courseId);
+    } else {
+      navigate("/login");
+    }
+  };
   return (
     <div className="SalesPage">
       <h1>Buy {course && course.name}</h1>
       <p>You're gonna love this course.</p>
-      <button>Buy Now</button>
+      <button onClick={buyOrLogin}>BUY NOW</button>
     </div>
   );
 };
 
 const mapState = (state, ownProps) => ({
+  currentUser: state.user.user,
   course: getCourseById(state, ownProps)
 });
 export default connect(mapState)(SalesPage);
